2d6d4c94175dad1316eccf25cd42919bc5f7adb6
Dmitriy Tolstykh <donimik46@gmail.com>
2023-10-05 20:31:45 +0300

    commit 8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,6 +13,21 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://demoqa.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize();
+        WebElement userNameInput = driver.findElement(By.xpath("//input[@id='userName']"));
+        WebElement userEmailInput = driver.findElement(By.xpath("//input[@id='userEmail']"));
+        WebElement userCurrentAddressInput = driver.findElement(By.xpath("//textarea[@id='currentAddress']"));
+        WebElement userPermanentAddressInput = driver.findElement(By.xpath("//textarea[@id='permanentAddress']"));
+        WebElement userConfirmButton = driver.findElement(By.xpath("//button[@id='submit']"));
+        userNameInput.sendKeys("Игорь");
+        userEmailInput.sendKeys("igor@mail.com");
+        userCurrentAddressInput.sendKeys("Курск");
+        userPermanentAddressInput.sendKeys("Kursk");
+        userConfirmButton.click();
+
+
+
     }
 }
